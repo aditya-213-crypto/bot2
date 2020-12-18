8006250add7c6501ae652b3cd5f3b7b38cb00d60
aditya-213-crypto <76215833+aditya-213-crypto@users.noreply.github.com>
2020-12-18 20:08:11 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  token: '1286881333:AAE-DV_hsGGkpbhplVQHei3SoRNZK_DL8UA', // token fromm @BotFather
+  mongoLink: 'https://cloud.mongodb.com/v2/5fc6333a3dd2f6706963ad79#clusters/edit?filter=starter', // mongo-link from cloud.mongodb.com
+  dev: '1107921506', // developer`s id for sending him/her errors
+  chauname: '@pokidipo',//1st channel username
+  chauname2: '',//2nd channel username
+  paycha: '',//payment channel username
+  payme:'-1001354401420',//payment channel I'd starts with -100
+  channel2: '',
+  channel: '',
+  botname:'Instant Pay',
+  apikey:'2pNVPRovkZbbodNM',
+  secret:'3ZhG7mYGMjulyzKvnkyerSkvHq3su8sB',
+  account:''
+}
