cea6dcd1cf30ce46d387d08de2cf50e9781f580c
aditya-213-crypto <76215833+aditya-213-crypto@users.noreply.github.com>
2020-12-19 14:24:38 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   token: '1286881333:AAE-DV_hsGGkpbhplVQHei3SoRNZK_DL8UA', // token fromm @BotFather
-  mongoLink: 'https://cloud.mongodb.com/v2/5fc6333a3dd2f6706963ad79#clusters/edit?filter=starter', // mongo-link from cloud.mongodb.com
+  mongoLink: 'mongodb+srv://User_1:arjuns1a2h3@cluster0.mfqk8.mongodb.net/aditya213?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
   dev: '1107921506', // developer`s id for sending him/her errors
   chauname: '@pokidipo',//1st channel username
   chauname2: '',//2nd channel username
